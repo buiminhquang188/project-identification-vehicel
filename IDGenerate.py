4e7b15a68d186301f26d5664b0c9d229b729b5ef
thanggtrann <thanggtrann12@gmail.com>
2022-01-14 21:05:46 +0700

    UDATE : GUI

--- IDGenerate.py
+++ IDGenerate.py
@@ -1,5 +1,6 @@
 Car_ID = []
 def idGenerate(data, color, ID):
+    Car_ID = []
     Car_ID.append(ID)
     Car_ID.append(data[:2])
     Car_ID.append(color)
--- main.py
+++ main.py
@@ -2,23 +2,57 @@ from CAM_1 import *
 from CAM_2 import *
 from IDGenerate import *
 from plateDetection import *
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import sys
 import mysql.connector
-ID = []
-if Detection("Main Images/1.jpg") in cam1List():
-    ID = idGenerate((cam1List()[1])[0], (cam1List()[1])[1], CAM_1_ID)
-    print(ID)
-elif  Detection("Main Images/1.jpg") in cam2List():
-    ID = idGenerate((cam2List()[1])[0], (cam2List()[1])[1], CAM_2_ID)
-    print(ID)
-else:
-    print("Car not here or may be system interupt")
-connection = mysql.connector.connect(host='b9llcrzvoxm6pbhe5xxn-mysql.services.clever-cloud.com',
-                                     database='b9llcrzvoxm6pbhe5xxn',
-                                     user='uijhoo8snd9qskzr',
-                                     password='svU4GQlGrl0KVHDf7cLQ')
-myCursor = connection.cursor()
-query = "INSERT INTO Car (cameraId, twoDigits , color, fourDigits) VALUES (%s, %s, %s, %s)"
-val = (str(ID[0]), str(ID[1]), str(ID[2]), str(ID[3]))
-myCursor.execute(query, val)
-connection.commit()
-print(myCursor.rowcount, "record inserted.")
+from datetime import datetime
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+folder = "Main Images"
+class UI(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi("Resource/mainWindow.ui", self)
+        self.startBut = self.findChild(QtWidgets.QPushButton, 'startButton')
+        self.path = self.findChild(QtWidgets.QComboBox, 'comboBox')
+        self.log = self.findChild(QtWidgets.QListWidget, 'listWidget')
+        self.image = self.findChild(QtWidgets.QGraphicsView, 'image')
+        self.startBut.clicked.connect(self.startFunc)
+        for path in os.listdir(folder):
+            full_path = os.path.join(folder, path)
+            if os.path.isfile(full_path):
+                self.path.addItem(full_path)
+        self.show()
+    def startFunc(self):
+        if Detection(self.path.currentText()) in cam1List():
+            ID = []
+            ID = idGenerate((cam1List()[1])[0], (cam1List()[1])[1], CAM_1_ID)
+            self.log.addItem(f'Current Time : {current_time}    Camera Name: {CAM_1_ID}  Infor : {str(ID[1]), str(ID[2]),str(ID[3])}')
+        elif Detection(self.path.currentText()) in cam2List():
+            ID = []
+            ID = idGenerate((cam2List()[1])[0], (cam2List()[1])[1], CAM_2_ID)
+            self.log.addItem(f'Current Time : {current_time}    Camera Name: {CAM_2_ID} Infor : {str(ID[1]), str(ID[2]),str(ID[3])} ')
+
+        else:
+            ID = []
+            msg = QtWidgets.QMessageBox()
+            msg.setIcon(QtWidgets.QMessageBox.Information)
+            msg.setText("Error")
+            msg.setInformativeText("Car not here or may be system interupt")
+            msg.setWindowTitle("Alert")
+            msg.exec_()
+            self.log.addItem("Car not here or may be system interupt")
+        connection = mysql.connector.connect(host='b9llcrzvoxm6pbhe5xxn-mysql.services.clever-cloud.com',
+                                             database='b9llcrzvoxm6pbhe5xxn',
+                                             user='uijhoo8snd9qskzr',
+                                             password='svU4GQlGrl0KVHDf7cLQ')
+        myCursor = connection.cursor()
+        query = "INSERT INTO Car (cameraId, twoDigits , color, fourDigits) VALUES (%s, %s, %s, %s)"
+        val = (str(ID[0]), str(ID[1]), str(ID[2]), str(ID[3]))
+        myCursor.execute(query, val)
+        connection.commit()
+        self.log.addItem(f'{myCursor.rowcount} record inserted.')
+app = QtWidgets.QApplication(sys.argv)
+window = UI()
+app.exec_()
