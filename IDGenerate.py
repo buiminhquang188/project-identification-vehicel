66b005a724838a6d3f5315c7cece4f891a6e5f94
Quang <buiminhquang188@gmail.com>
2022-02-13 12:08:11 +0700

    update

--- IDGenerate.py
+++ IDGenerate.py
@@ -4,5 +4,5 @@ def idGenerate(data, color, ID):
     Car_ID.append(ID)
     Car_ID.append(data[:2])
     Car_ID.append(color)
-    Car_ID.append(data[-4:])
+    Car_ID.append(data[-5:])
     return Car_ID
